4cfd876ce6ad3f419ad0916c79b7911c27f017ae
kurbatovbn <warkb@yandex.ru>
2018-02-21 16:36:08 +0400

    Написал модель, представления и шаблон для вывода списка добавленных голосовалок

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pols.apps.PolsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
