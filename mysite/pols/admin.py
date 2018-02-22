b8e9e18b5c30c84977925fb5d9d429d3bfacd1ce
kurbatovbn <warkb@yandex.ru>
2018-02-22 15:50:48 +0400

    Написал добавление вопросов.

--- mysite/pols/admin.py
+++ mysite/pols/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+#admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date',
+        'was_published_recently')
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'],
+            'classes': ['collapse']})
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 # Register your models here.
+admin.site.register(Question, QuestionAdmin)
